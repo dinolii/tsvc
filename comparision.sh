a6f2589c4f1668f478758087654a7ee8be76ea3b
dino li <dino.li@mail.utoronto.ca>
2022-04-04 00:23:55 -0400

    Add files via upload

---
+++ comparision.sh
@@ -0,0 +1,24 @@
+rm gnu-baseline.txt
+touch gnu-baseline.txt
+echo "******** Start Base Line ********"
+for i in `seq 1 5`
+do
+    echo $i
+   ./GNU-baseline/tsvc_novec_default >>gnu-baseline.txt
+done
+rm gnu-sse.txt
+touch gnu-sse.txt
+echo "******** Start SSE ********"
+for i in `seq 1 5`
+do
+    echo $i
+   ./GNU-sse/tsvc_novec_default >>gnu-sse.txt
+done
+rm gnu-avx.txt
+touch gnu-avx.txt
+echo "******** Start AVX ********"
+for i in `seq 1 5`
+do
+    echo $i
+   ./GNU-avx/tsvc_novec_default >>gnu-avx.txt
+done
