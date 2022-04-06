967cdf35e6dd0e44b739de2a589fd53c148fda64
Junming <peterzhangjm@outlook.com>
2022-04-06 14:31:17 -0400

    Fix collection script

--- bin/collect-novec.sh
+++ bin/collect-novec.sh
@@ -1,6 +1,5 @@
-rm intel-baseline.txt
-touch intel-baseline.txt
-names=("s123" "s124" "s161" "s1161" "s253" "s258" "s271" "s273" "s274" "s277" "s278" "s2711" "s2712" "s314" "s315" "s316" "s3111" "s3113" "s341" "s342" "s343" "s443" "vif")
+#names=("s123" "s124" "s161" "s1161" "s253" "s258" "s271" "s273" "s274" "s277" "s278" "s2711" "s2712" "s314" "s315" "s316" "s3111" "s3113" "s341" "s342" "s343" "s443" "vif")
+names=($2)
 for name in "${names[@]}"
 do
 	  for i in `seq 1 10`
